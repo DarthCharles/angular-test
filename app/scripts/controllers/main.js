9e27fb22c280d7b6882e38804bc6b21569dd3f24
DarthCharles <carlos.darth@gmail.com>
2015-01-18 19:46:35 -0700

    Agregada una lista de pendientes y funcionalidades a los botones add y delete

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,13 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name angularTestApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the angularTestApp
- */
 angular.module('angularTestApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+.controller('MainCtrl', function ($scope) {
+	$scope.todos = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+    $scope.addTodo = function () {
+      $scope.todos.push($scope.todo);
+      $scope.todo = '';
+    };
+    $scope.removeTodo = function (index) {
+    	$scope.todos.splice(index,1);
+    };
+});
